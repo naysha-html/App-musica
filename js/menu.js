ebe41feb7f9710a6f0034ee61b6f8917ac582264
Naysha Choquejahua <fchoquejahua@gmail.com>
2021-08-23 20:16:10 -0500

    make it better

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-let menu_btn = document.getElementById('menu-btn');
-let menu_list = document.getElementById('menu-list')
-menu_btn.addEventListener('click',()=>{
-    menu_list.classList.toggle('active-menu')
-})
